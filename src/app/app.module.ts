18acf4124701b7e24f30de5b100487ba31e4e052
Alvaro <0alvarorm0@gmail.com>
2020-04-17 21:21:20 +0100

    Chapter 7 Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import {ServersComponent} from './servers/servers.component';
+import {ServerComponent} from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CockpitComponent,
-    ServerElementComponent
+    ServersComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,7 +15,7 @@ export class ServersComponent implements OnInit {
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    },2000);
+    }, 2000);
   }
 
   ngOnInit(): void {
